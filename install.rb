ee1ac54e0754919290b1cd2be1c287d27ae3ded1
Heiko Zeus <heiko.zeus@zweitag.de>
2013-09-11 13:13:17 +0200

    Correct install script

--- install.rb
+++ install.rb
@@ -1,4 +1,5 @@
 #! /usr/bin/ruby
-Dir['.*'].each do |file|
-  system "ln -s #{File.expand_path(file)} ~"
+dir = File.expand_path(File.dirname(__FILE__))
+Dir["#{dir}/.*"].each do |file|
+  system "ln -s #{File.expand_path(file)} ~" unless File.directory?(file)
 end
